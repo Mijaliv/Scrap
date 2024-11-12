f7dae92813d1dcd8f1c6d4d2f9e67470b3bfdfaa
Mijalivananunez <mijalivananunez@hotmail.com>
2024-11-12 12:36:24 -0300

    Modificacion en jornada

--- ScrapCompuTrab.py
+++ ScrapCompuTrab.py
@@ -5,7 +5,7 @@ from requests.exceptions import HTTPError, Timeout
 def make_request(url, headers=None):
     try:
         response = requests.get(url, headers=headers or {}, timeout=10)
-        response.raise_for_status()  # Verifica si la solicitud fue exitosa (código 200)
+        response.raise_for_status()
         print(response.status_code)
         return response.text
     except HTTPError as error:
@@ -16,62 +16,51 @@ def make_request(url, headers=None):
         print(f"Error: {error}")
 
 def scrape_jobs(url, headers):
-    # Hacer la solicitud a la página
     html_content = make_request(url, headers)
     if not html_content:
         return []
 
-    # Parsear el contenido HTML con BeautifulSoup
     soup = BeautifulSoup(html_content, 'lxml')
     
-    # Extraer los empleos
     jobs = []
-    # Buscar cada tarjeta de empleo en la página
     for job_elem in soup.find_all('article', class_='box_offer'):
-        # Extraer el título del empleo
         title_elem = job_elem.find('a', class_='js-o-link')
         title = title_elem.text.strip() if title_elem else 'No especificado'
 
-        # Extraer el enlace del empleo
         link = title_elem['href'] if title_elem else '#'
         full_link = f"https://ar.computrabajo.com{link}"
 
-        # Extraer la ubicación
         location_elem = job_elem.find('p', class_='fs16 fc_base mt5')
         location = location_elem.text.strip() if location_elem else 'Ubicación no especificada'
 
-        # Extraer el nombre de la empresa
         empresa_elem = job_elem.find('a', class_='fc_base t_ellipsis')
-        empresa_elem = empresa_elem.text.strip() if empresa_elem else 'Empresa no especificada'
+        empresa = empresa_elem.text.strip() if empresa_elem else 'Empresa no especificada'
 
-        # Extraer la fecha de publicación
         date_elem = job_elem.find('p', class_='fs13 fc_aux mt15')
         date = date_elem.text.strip() if date_elem else 'Fecha no especificada'
 
-        #Extrae el salario
-        salario_elem = job_elem.find('div',class_='fs13 mt15')
-        salario_elem = salario_elem.text.strip() if salario_elem else 'Salario no especificado'
-        #IMPLEMENTAR JORNADA Anda pero raro
-        jornada_elem = job_elem.find('span',class_='dIB mr10')
-        jornada_elem = jornada_elem.text.strip() if jornada_elem else 'Jornada no especificada'
-        #IMPLEMENTAR EVALUACION DE LAS EMPRESAS
+        salario_elem = job_elem.find('div', class_='fs13 mt15')
+        salario = salario_elem.text.strip() if salario_elem else 'Salario no especificado'
+
+        jornada_elem = job_elem.find('span', class_='dIB mr10')
+        jornada = jornada_elem.text.strip() if jornada_elem else 'Jornada no especificada'
         
         # Agregar los detalles del empleo a la lista
         jobs.append({
             'title': title,
             'link': full_link,
             'location': location,
-            'empresa': empresa_elem,
+            'empresa': empresa,
             'date': date,
-            'salario': salario_elem,
-            'jornada': jornada_elem
+            'salario': salario,
+            'jornada': jornada,
         })
     
     return jobs
 
 # Definir el User-Agent
 agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41"
-url = "https://ar.computrabajo.com/trabajo-de-vendedor"
+url = "https://ar.computrabajo.com/trabajo-de-vendedor?p=2"
 
 # Scrapeando los empleos
 empleos = scrape_jobs(url, {"User-Agent": agent})
@@ -83,14 +72,6 @@ for empleo in empleos:
     print(f"Ubicación: {empleo['location']}")
     print(f"Empresa: {empleo['empresa']}")
     print(f"Fecha de publicación: {empleo['date']}")
-    print(f"Salario:{empleo['salario']}")
-    print(f"Jornada:{empleo['jornada']}")
+    print(f"Salario: {empleo['salario']}")
+    print(f"Jornada: {empleo['jornada']}")
     print('---')
-
-
-
-
-
-
-
-
